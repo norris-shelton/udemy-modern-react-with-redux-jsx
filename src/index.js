fdbec4078d0df2a835662eee6d47c5f99449c1d2
norris-shelton <norris.shelton@gmail.com>
2026-07-01 15:51:20 -0400

    Separated into App.js and index.js for better organization.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 // 1) Import the React and ReactDOM libraries
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
 
 // 2) Get a reference to the div with ID root
-const el = document.getElementById('root');
+const el = document.getElementById('root')
 
 // 3) Tell React to take control of that element
-const root = ReactDOM.createRoot(el);
+const root = ReactDOM.createRoot(el)
 
-// 4) Create a component
-function App() {
-    return (
-        <div className="wrapper">
-            <textarea readOnly maxLength={3} spellCheck style={{backgroundColor: 'gray'}}/>
-        </div>
 
-    )
-}
-
-// 5) Show the component on the screen
+// 4) Show the component on the screen
 root.render(<App />);
